d57371bde15fe40dbe3f2da175b5dedff927d784
Jubair <mjahmed.wd@gmail.com>
2021-11-25 16:51:48 +0600

    11/25/21

--- middlewares/users/userValidators.js
+++ middlewares/users/userValidators.js
@@ -1,65 +1,90 @@
+// external imports
 const { check, validationResult } = require("express-validator");
 const createError = require("http-errors");
 const path = require("path");
 const { unlink } = require("fs");
 
+// internal imports
+const User = require("../../models/People");
+
+// add user
 const addUserValidators = [
   check("name")
     .isLength({ min: 1 })
+    .withMessage("Name is required")
     .isAlpha("en-US", { ignore: " -" })
-    .withMessage("Name field is required")
+    .withMessage("Name must not contain anything other than alphabet")
     .trim(),
   check("email")
     .isEmail()
-    .withMessage("Email field is required")
+    .withMessage("Invalid email address")
     .trim()
     .custom(async (value) => {
       try {
         const user = await User.findOne({ email: value });
         if (user) {
-          throw createError("Email already exists");
+          throw createError("Email already is use!");
         }
       } catch (err) {
         throw createError(err.message);
       }
     }),
   check("mobile")
-    .isMobilePhone("bn-BD", { strictMode: true })
-    .withMessage("Mobile field is required")
+    .isMobilePhone("bn-BD", {
+      strictMode: true,
+    })
+    .withMessage("Mobile number must be a valid Bangladeshi mobile number")
     .custom(async (value) => {
       try {
         const user = await User.findOne({ mobile: value });
         if (user) {
-          throw createError("Mobile already exists");
+          throw createError("Mobile already is use!");
         }
       } catch (err) {
         throw createError(err.message);
       }
     }),
-  check("password").isStrongPassword().withMessage("Password is weak"),
+  check("password")
+    .isStrongPassword()
+    .withMessage(
+      "Password must be at least 8 characters long & should contain at least 1 lowercase, 1 uppercase, 1 number & 1 symbol"
+    ),
 ];
 
-const addUserValidationHandler = (req, res, next) => {
+const addUserValidationHandler = function (req, res, next) {
   const errors = validationResult(req);
   const mappedErrors = errors.mapped();
-  if (object.keys(mappedErrors).length > 0) {
+  if (Object.keys(mappedErrors).length === 0) {
     next();
   } else {
     // remove uploaded files
     if (req.files.length > 0) {
-      const { fileName } = req.files[0];
+      const { filename } = req.files[0];
       unlink(
-        path.join(__dirname, `/../public/uploads/avatars/${fileName}`),
+        path.join(__dirname, `/../public/uploads/avatars/${filename}`),
         (err) => {
           if (err) console.log(err);
         }
       );
     }
 
+    // response the errors
     res.status(500).json({
-      errors: mappedErrors
-    })
+      errors: mappedErrors,
+    });
   }
 };
 
-module.exports = { addUserValidators, addUserValidationHandler };
+async function removeUser(req, res, next) {
+  try {
+    const user = await User.findByIdAndDelete({_id: req.params.id});
+   
+  } catch (err) {
+    
+  }
+}
+
+module.exports = {
+  addUserValidators,
+  addUserValidationHandler,
+};
